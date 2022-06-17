415552a77dab3e324f40379e54799a23918ab2e9
YuyaMurakami <60170610+yuyamurakami-projects@users.noreply.github.com>
2022-06-17 16:22:41 +0900

    Initial commit

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+export const hello = (name: string) => {
+  console.log(name)
+}
+
+export const hello2 = (name: string) => {
+  console.log(name)
+}
+
+export const hello3 = (name: string) => {
+  console.log(name)
+}
+export const hello4 = (name: string) => {
+  console.log(name)
+}
